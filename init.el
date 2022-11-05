9c06f92b33f1deab244522298f340e93c2855742
Senjaya <amarpanjis@gmail.com>
2022-11-05 23:50:44 +0700

    move use-package initialization to top

--- init.el
+++ init.el
@@ -1,3 +1,17 @@
+;; package configuration
+;; ==============================================================
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
+(package-initialize)
+(setq use-package-verbose t)
+;; This is only needed once, near the top of the file
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "<path where use-package is installed>")
+  (require 'use-package))
+
 (defun amay/open-terminal()
   "open term"
   (interactive)
@@ -182,15 +196,6 @@
 (set-face-attribute 'font-lock-function-name-face nil :weight 'semibold)
 
 (set-face-attribute 'mode-line-inactive nil :background "#39363b")
-
-;; package configuration
-;; ==============================================================
-(require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")))
-(package-initialize)
-(setq use-package-verbose t)
 
 ;;(use-package almost-mono-themes
 ;;  :config
